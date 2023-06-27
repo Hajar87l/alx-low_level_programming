81043c5d89963969553f024adde0f50ddb84c152
Hajar87l <skhajar39@gmail.com>
2023-06-27 10:50:08 -0700

    puts

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,23 +9,23 @@
  */
 int main(void)
 {
-int i;
-char password[10]; // assuming the password length is 10 characters
+    int i;
+    char password[10]; // assuming the password length is 10 characters
 
-/* seed random number generator */
-srand(time(0));
+    /* seed random number generator */
+    srand(time(0));
 
-/* generate a random password */
-for (i = 0; i < 9; i++)
-{
-password[i] = 'A' + rand() % ('Z'-'A' + 1);
-}
+    /* generate a random password */
+    for (i = 0; i < 9; i++)
+    {
+        password[i] = 'A' + rand() % ('Z'-'A' + 1);
+    }
 
-/* Null-terminate the password string */
-password[9] = '\0';
+    /* Null-terminate the password string */
+    password[9] = '\0';
 
-/* Output the password */
-printf("%s\n", password);
+    /* Output the password */
+    printf("%s\n", password);
 
-return (0);
+    return (0);
 }
