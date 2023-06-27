a4d5a070e62ada31c807134402bc7c838ca5e0c6
Hajar87l <skhajar39@gmail.com>
2023-06-26 18:40:25 -0700

    puts

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -10,6 +10,9 @@ int main(void)
     char *str;
 
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    
+    /* Call the print_rev function with the string as argument */
     print_rev(str);
+
     return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - Prints a string in reverse followed by a new line to stdout
- * @s: The string to be printed
- *
- * Return: void
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: The string to be printed in reverse.
  */
 void print_rev(char *s)
 {
 int length = 0;
-int i;
 
 /* Find the length of the string */
-while (s[length])
+while (s[length] != '\0')
 {
 length++;
 }
 
-/* Print the string in reverse */
-for (i = length - 1; i >= 0; i--)
+/* Print the characters in reverse */
+for (length = length - 1; length >= 0; length--)
 {
-_putchar(s[i]);
+_putchar(s[length]);
 }
 
 /* Print a new line */
