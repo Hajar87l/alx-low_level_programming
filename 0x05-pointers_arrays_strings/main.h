989117528b99a874eb0b8d4775be9fe2527c9d3c
Hajar87l <skhajar39@gmail.com>
2023-06-26 18:46:31 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_rev(char *s);
+/* Function to print characters */
 int _putchar(char c);
+
+/* Function to print every other character of a string */
+void puts2(char *str);
 
 #endif /* MAIN_H */
