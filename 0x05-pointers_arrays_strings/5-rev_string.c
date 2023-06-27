e5ab3bc6e5841aa91799601e989f61d96ac402cb
Hajar87l <skhajar39@gmail.com>
2023-06-26 18:33:08 -0700

    puts

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,21 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int i;
-    char temp;
+int length = 0;
+int i;
+char temp;
 
-    /* Find the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+/* Find the length of the string */
+while (s[length] != '\0')
+{
+length++;
+}
 
-    /* Reverse the string */
-    for (i = 0; i < length / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[length - i - 1];
-        s[length - i - 1] = temp;
-    }
+/* Reverse the string */
+for (i = 0; i < length / 2; i++)
+{
+temp = s[i];
+s[i] = s[length - i - 1];
+s[length - i - 1] = temp;
+}
 }
