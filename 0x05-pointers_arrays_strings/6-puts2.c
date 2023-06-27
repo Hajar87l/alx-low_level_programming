8d766f27ddf518f5d7bd3bf7cd35299a7ce4385f
Hajar87l <skhajar39@gmail.com>
2023-06-27 08:56:50 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,8 @@
 /**
  * puts2 - prints every other character of a string
  * @str: pointer to the string to print
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
@@ -12,5 +14,6 @@ for (i = 0; str[i] != '\0'; i += 2)
 {
 _putchar(str[i]);
 }
+	
 _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function to print characters */
 int _putchar(char c);
-
-/* Function to print every other character of a string */
 void puts2(char *str);
 
 #endif /* MAIN_H */
