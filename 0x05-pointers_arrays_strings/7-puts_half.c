872786643d7f9820dcdee94179352651668595f1
Hajar87l <skhajar39@gmail.com>
2023-06-27 09:17:19 -0700

    puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,24 +8,24 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i;
-    int start;  /* Moved declaration to the top of the block */
+int length = 0;
+int i;
+int start;  /* Moved declaration to the top of the block */
 
-    /* Find the length of the string */
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+/* Find the length of the string */
+while (str[length] != '\0')
+{
+length++;
+}
 
-    /* Calculate the start index */
-    start = (length % 2 == 0) ? length / 2 : (length + 1) / 2; /* Removed declaration */
+/* Calculate the start index */
+start = (length % 2 == 0) ? length / 2 : (length + 1) / 2; /* Removed declaration */
 
-    /* Print the second half of the string */
-    for (i = start; i < length; i++)
-    {
-        _putchar(str[i]);
-    }
+/* Print the second half of the string */
+for (i = start; i < length; i++)
+{
+_putchar(str[i]);
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
