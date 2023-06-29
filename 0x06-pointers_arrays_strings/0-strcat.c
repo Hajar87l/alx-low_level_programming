42907b55180285b2450bb005c66c79cf634f4028
Hajar87l <skhajar39@gmail.com>
2023-06-29 10:39:54 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,24 +9,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+int i = 0, j = 0;
 
-    /* Find the end of the dest string */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+/* Find the end of the dest string */
+while (dest[i] != '\0')
+{
+i++;
+}
 
-    /* Copy src to dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+/* Copy src to dest */
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
-    /* Add the null byte at the end */
-    dest[i] = '\0';
+/* Add the null byte at the end */
+dest[i] = '\0';
 
-    return (dest);
+return (dest);
 }
