51acb13d32486612dc794e34a64cd8089e78a4b8
Hajar87l <skhajar39@gmail.com>
2023-06-29 10:48:12 -0700

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,24 +10,24 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0, j = 0;
+int i = 0, j = 0;
 
-    /* Find the end of dest string */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+/* Find the end of dest string */
+while (dest[i] != '\0')
+{
+i++;
+}
 
-    /* Copy at most n bytes from src to dest */
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+/* Copy at most n bytes from src to dest */
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
-    /* Null terminate dest */
-    dest[i] = '\0';
+/* Null terminate dest */
+dest[i] = '\0';
 
-    return dest;
+return dest;
 }
