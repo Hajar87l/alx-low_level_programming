1fcd013a1669459dae7aed41c05deda3c6736d29
Hajar87l <skhajar39@gmail.com>
2023-06-30 05:00:28 -0700

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Prints the message */
+puts("\"Programming is like building a multilingual puzzle");
+
+/* Return 0 */
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Prints the message */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+/* Return 0 */
+return (0);
+}
